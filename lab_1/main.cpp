ac5c3d7c8af1d4f9c9bb83d9bdc546710d4c6bbe
5anji <gitlanm@gmail.com>
2023-02-06 11:28:55 +0200

    edit input values

--- lab_1/main.cpp
+++ lab_1/main.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 
 boost::multiprecision::uint1024_t Fibonacci_a(size_t limit) {
-
     boost::multiprecision::uint1024_t result = 0;
     std::pair<boost::multiprecision::uint1024_t, boost::multiprecision::uint1024_t> container;
 
@@ -37,12 +36,10 @@ boost::multiprecision::uint1024_t Fibonacci_b(size_t limit) {
 }
 
 boost::multiprecision::uint1024_t Fibonacci_c(size_t limit) {
-
     return static_cast<boost::multiprecision::uint1024_t>((pow((1 + sqrt(5)) / 2, limit) - pow((1 - sqrt(5)) / 2, limit)) / sqrt(5));
 }
 
 boost::multiprecision::uint1024_t Fibonacci_d(size_t limit) {
-
     boost::multiprecision::uint1024_t a = 0, b = 1, result = 0, row;
 
     while (limit >= 2) {
@@ -56,7 +53,6 @@ boost::multiprecision::uint1024_t Fibonacci_d(size_t limit) {
 }
 
 boost::multiprecision::uint1024_t Fibonacci_e(size_t limit) {
-
     if ((limit == 0) || (limit == 1)) {
         return limit;
     } else {
@@ -65,7 +61,6 @@ boost::multiprecision::uint1024_t Fibonacci_e(size_t limit) {
 }
 
 boost::multiprecision::uint1024_t Fibonacci_f(size_t limit) {
-
     std::vector<size_t> temp{0, 1};
     while (temp.size() <= limit) {
         auto end = temp.size();
@@ -77,26 +72,34 @@ boost::multiprecision::uint1024_t Fibonacci_f(size_t limit) {
 void display_timed(std::function<boost::multiprecision::uint1024_t(size_t)> f, size_t limit) {
     clock_t start = clock();
 
-    std::cout << "   Result is " << f(limit) << '\t';
+    std::cout << f(limit) << '\t';
     double timer = static_cast<double>(clock() - start) / CLOCKS_PER_SEC;
     std::cout << "Time taken: " << std::fixed << std::setprecision(6) << timer << std::endl;
 }
 
 int main() {
+    size_t arr1[] = {5, 7, 10, 12, 15, 17, 20, 22, 25, 27, 30, 32, 35, 37, 40, 42, 45};
+    size_t arr2[] = {501, 631, 794, 1000, 1259, 1585, 1995, 2512, 3162, 3981, 5012, 6310, 7943, 10000, 12589, 15849};
+
     std::vector<std::function<boost::multiprecision::uint1024_t(size_t)>> Fibonacci_calc_functions = {
             Fibonacci_a,
             Fibonacci_b,
-            Fibonacci_c,
+            // Fibonacci_c,
             Fibonacci_d,
-            Fibonacci_e,
             Fibonacci_f};
 
-    size_t limit;
-    std::cout << "Input limit: ";
-    std::cin >> limit;
+    for (auto&& i : Fibonacci_calc_functions) {
+        for (auto&& limit : arr2) {
+            std::cout << "f(" << limit << ") = ";
+            display_timed(i, limit);
+        }
 
-    for (auto&& i : Fibonacci_calc_functions)
-        display_timed(i, limit);
+        std::cout << std::endl;
+    }
 
+    for (auto&& limit : arr1) {
+        std::cout << "f(" << limit << ") = ";
+        display_timed(Fibonacci_e, limit);
+    }
     return 0;
 }
